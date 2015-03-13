af38d5c4fcd5039af755293fbbe90a11e5272255
Marcelo Gutierrez <mmgc84@gmail.com>
2015-03-13 12:25:50 -0600

    Add __unicode__() to Question and Choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,9 +6,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __unicode__(self):
+        return self.question_text
+
 
 # Choice model
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text__
