8a86bc38c58656147c582ba47ab738ea8b8ae48e
Marcelo Gutierrez <mmgc84@gmail.com>
2015-03-14 15:18:06 -0600

    index view rewritten

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
+
 
 from polls.models import Question
 
@@ -8,11 +8,8 @@ from polls.models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
