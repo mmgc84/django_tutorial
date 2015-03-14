b3ad731834893b8cd415842571a8786734d20938
Marcelo Gutierrez <mmgc84@gmail.com>
2015-03-14 10:32:04 -0600

    Update indentation style polls/urls.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 
 from polls import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index'), )
+urlpatterns = patterns('',
+                       url(r'^$', views.index, name='index'),
+                       )
