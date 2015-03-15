552855b65184959f2377e7c80c9dd0821a8a7e0c
Marcelo Gutierrez <mmgc84@gmail.com>
2015-03-15 11:41:22 -0600

    Fixed class IndexView(generic.ListView)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,6 +9,11 @@ from polls.models import Question, Choice
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DeleteView):
